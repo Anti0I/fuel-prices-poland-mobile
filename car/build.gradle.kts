86cf667701a028f646f707c1b7be1ced33d92685
Jasiek <u119_jangaw_waw@technischools.com>
2026-05-10 14:00:29 +0200

    feat: add OkHttp, coroutines dependencies and INTERNET permission

--- car/build.gradle.kts
+++ car/build.gradle.kts
@@ -43,6 +43,8 @@ dependencies {
     implementation(libs.androidx.car.app.automotive)
     implementation(libs.gson)
     implementation(libs.play.services.location)
+    implementation(libs.okhttp)
+    implementation(libs.kotlinx.coroutines.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
